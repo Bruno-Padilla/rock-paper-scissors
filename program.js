64b8e14f955fd3eab05268aabbad25694b53bd5b
Brunniel <brunisdan5@gmail.com>
2026-01-10 23:41:43 -0600

    game done

--- program.js
+++ program.js
@@ -8,30 +8,48 @@ function getHumanChoice() {
     return prompt("rock/paper/scissors").toLowerCase();
 }
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-
 function playRound(humanChoice, computerChoice) {
     switch (humanChoice) {
         case "rock":
-            if (computerChoice === "rock") 
-                alert("Rock vs Rock. It's a draw!");
-            else if (computerChoice === "paper")
-                alert("Rock vs Paper. You Lose!");
-            else
-                alert("Rock vs Scissors. You win!");    
+            if (computerChoice === "rock") return "Rock vs Rock. It's a draw!";
+            else if (computerChoice === "paper") return "Rock vs Paper. You Lose!";
+            else return "Rock vs Scissors. You Win!";    
             break;
 
         case "paper":
-            if (computerChoice === "rock") alert("Paper vs Rock. You win!");
-            else if (computerChoice === "paper") alert("Paper vs Paper. It's a draw!");
-            else alert("Paper vs Scissors. You Lose!");    
+            if (computerChoice === "rock") return "Paper vs Rock. You win!";
+            else if (computerChoice === "paper") return "Paper vs Paper. It's a draw!";
+            else return "Paper vs Scissors. You Lose!";    
             break;
 
         case "scissors":
-            computerChoice === "scissors" ? alert("Scissors vs Rock. You Lose!") : computerChoice === "paper" ? alert("Scissors vs Paper. You Win!") : alert("Scissors vs Scissors. It's a draw!");    
+            if (computerChoice === "rock") return "Scissors vs Rock. You Lose!"
+            else if (computerChoice === "paper") return "Scissors vs Paper. You Win!"
+            else return "Scissors vs Scissors. It's a draw!";    
             break;
     }
 }
 
-playRound(humanSelection, computerSelection);
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i=0; i<5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        let result = playRound(humanChoice, computerChoice);
+        alert(result);
+
+        if (result.toLowerCase().includes("win")) ++humanScore
+        else if (result.toLowerCase().includes("lose")) ++computerScore
+    }
+
+    alert(getWinner(humanScore, computerScore));
+}
+
+function getWinner(humanScore, computerScore) {
+    if (humanScore > computerScore) return "Absolute winner: Human!!!"
+    else return "Absolute winner: Computer!!!"
+}
+
+playGame();
