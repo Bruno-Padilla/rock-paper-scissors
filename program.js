7225b1a180f711023875de4ecfbaff72ece7d728
Brunniel <brunisdan5@gmail.com>
2026-01-10 22:33:35 -0600

    getComputerChoice function done

---
+++ program.js
@@ -0,0 +1,5 @@
+function getComputerChoice() {
+    const options = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
