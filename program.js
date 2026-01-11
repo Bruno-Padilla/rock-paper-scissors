d9a65dbc7eb258b3f94a5d2c47ca92699e08ed83
Brunniel <brunisdan5@gmail.com>
2026-01-10 23:06:03 -0600

    playRound function done

--- program.js
+++ program.js
@@ -7,3 +7,31 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("rock/paper/scissors").toLowerCase();
 }
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "rock") 
+                alert("Rock vs Rock. It's a draw!");
+            else if (computerChoice === "paper")
+                alert("Rock vs Paper. You Lose!");
+            else
+                alert("Rock vs Scissors. You win!");    
+            break;
+
+        case "paper":
+            if (computerChoice === "rock") alert("Paper vs Rock. You win!");
+            else if (computerChoice === "paper") alert("Paper vs Paper. It's a draw!");
+            else alert("Paper vs Scissors. You Lose!");    
+            break;
+
+        case "scissors":
+            computerChoice === "scissors" ? alert("Scissors vs Rock. You Lose!") : computerChoice === "paper" ? alert("Scissors vs Paper. You Win!") : alert("Scissors vs Scissors. It's a draw!");    
+            break;
+    }
+}
+
+playRound(humanSelection, computerSelection);
